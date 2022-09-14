d4878754c95a1782a108bb72d60a0340ff8fea5d
= <=>
2022-09-13 21:29:08 -0300

    first commit

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 const loginbutton = document.getElementById("cadastrar");
 loginbutton.addEventListener("click", function(){
-    window.location.replace('../start/cadastro/cadastrar.html')
+    window.location.replace('../cadastro/cadastrar.html')
 });
 
 const cadastrobutton = document.getElementById("login");
 cadastrobutton.addEventListener("click", function(){
-    window.location.replace('../start/login/login.html')
+    window.location.replace('../login/login.html')
 });
