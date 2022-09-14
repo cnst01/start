0d4e183e5c38c624daa854cf3c0d22aacb07cadb
= <=>
2022-09-13 21:37:43 -0300

    servertest1

--- server/users.js
+++ server/users.js
@@ -1,4 +1,4 @@
-export default users = [
+export const users = [
     {username: 'user Name', email: 'user Email', password: '8digitsPassword'},
     {username: 'Cassio', email: 'cassionst@gmail.com', password: '12345678'} 
 ]
