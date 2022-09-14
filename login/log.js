10d7ff2800dde7b596273cb86f8299c76f8f5210
= <=>
2022-09-14 20:51:15 -0300

    servertest1

--- login/log.js
+++ login/log.js
@@ -2,3 +2,10 @@ const voltar = document.getElementById("voltar");
 voltar.addEventListener("click", function(){
     window.location.replace('../index.html')
 });
+
+const enviar = document.getElementById('login')
+enviar.addEventListener('click', function(){
+    const username = document.getElementById('username')
+    const password = document.getElementById('password')
+    console.log('Trying login with: ' + username + ' , ' + password)
+})
